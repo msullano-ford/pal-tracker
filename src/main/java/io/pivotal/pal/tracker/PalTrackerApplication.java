f74fd42499db11d7f0adbe8793fd9fe6fee54243
Manny Sullano <msullano@ford.com>
2019-05-07 08:46:34 -0600

    added comments

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Trigger build
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main (String[] args ) {
